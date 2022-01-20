4264064e400f33a6b2c22490cd010f6b71f858a1
Charles Tudor <mail@ctudor.net>
2022-01-20 12:44:27 +0000

    Added new tests for strange player names

--- src/poker_now_log_converter/utils.py
+++ src/poker_now_log_converter/utils.py
@@ -4,6 +4,13 @@
 currencyCCToSymbol = dict(USD='$', CAD='$', GBP='£', EUR='€', SEK='kr', PLY='P')
 
 
+def open_text_file(path: str):
+    with open(path, encoding="utf-8") as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+    return lines
+
+
 class PNLogParsingException(Exception):
     def __init__(self, message):
         super().__init__(message)
